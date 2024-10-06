946cb3bb20ed12a753bc48ff2c255e1ccd0ed455
JetGreen <grishadzyin@gmail.com>
2024-10-06 19:49:32 +0500

    carousel

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <template>
+  <v-carousel hide-delimiters cycle>
+    <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover></v-carousel-item>
+
+    <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg" cover></v-carousel-item>
+
+    <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover></v-carousel-item>
+  </v-carousel>
+
   <h1>Это самый большой заголовок</h1>
   <h2>Это заголовок поменьше</h2>
-  <h3>Это акцентный текст</h3>  
+  <h3>Это акцентный текст</h3>
 </template>
 <style scoped lang="scss"></style>
