e8ae36eb97c8224684ec472177607f026ba5642b
JetGreen <grishadzyin@gmail.com>
2024-10-06 19:35:29 +0500

    add navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,25 @@ const router = useRouter()
 </script>
 <template>
   <v-app>
-    <v-app-bar :elevation="0" height="40"> ещё один хедер </v-app-bar>
-    <v-app-bar :elevation="0">
+    <v-app-bar scroll-behavior="fully-hide" :elevation="0" height="40" color="#E0D5C3" > ещё один хедер </v-app-bar>
+    <v-app-bar :elevation="0" height="80">
       <v-row>
-        <v-col cols="12" class="d-flex justify-center">
+        <v-col cols="12" class="d-flex justify-center align-center">
           <img class="logo" @click="router.push('/')" src="" alt="логотип" />
-          <div class="desktop-navigation d-md-flex d-none">Виды массажа Расписание Стоимость Обучение О нас</div>
+          <div class="desktop-navigation d-md-flex d-none">
+            <div class="nav-item border-right">
+              <NuxtLink to="/sdf"> Виды массажа </NuxtLink>
+            </div>
+            <div class="nav-item border-right">
+              <NuxtLink to="/schedule"> Расписание </NuxtLink>
+            </div>
+            <div class="nav-item border-right">
+              <NuxtLink to="/price"> Стоимость </NuxtLink>
+            </div>
+            <div class="nav-item">
+              <NuxtLink> Обучение </NuxtLink>
+            </div>
+          </div>
           <div class="phone-navigation d-flex d-md-none">on phone</div>
         </v-col>
       </v-row>
@@ -17,7 +30,7 @@ const router = useRouter()
     <v-main>
       <slot />
       тут будет подвал
-      <h3>Основные цвета: </h3>
+      <h3>Основные цвета:</h3>
       <v-sheet color="#809248" height="50" width="50"></v-sheet>
       <v-sheet color="#E0D5C3" height="50" width="50"></v-sheet>
       <v-sheet color="#5C5B58" height="50" width="50"></v-sheet>
@@ -32,6 +45,16 @@ const router = useRouter()
   cursor: pointer;
 }
 .desktop-navigation {
+  color: #809248;
+  .nav-item {
+    margin: 10px;
+    margin-right: 0;
+    padding: 13px 0px;
+    padding-right: 10px;
+  }
+  .border-right {
+    border-right: 1px solid rgba(128, 146, 72, 0.2);
+  }
 }
 .phone-navigation {
 }
