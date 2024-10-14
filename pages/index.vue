7aa95d7a8cef5c4198f4ab24473198d676435a25
solomon <snisimov2005@mail.ru>
2024-10-14 19:21:12 +0500

    niga

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,32 @@
 <script lang="ts" setup>
-const router = useRouter()
+const router = useRouter();
+
+const items = [
+  {
+    title: "выаав",
+    subtitle: "ыавав",
+    image:
+      "https://i.pinimg.com/736x/13/e8/86/13e886fbec6287235522bb2a81b35f97.jpg",
+  },
+  {
+    title: "выаав",
+    subtitle: "ыавав",
+    image:
+      "https://i.pinimg.com/736x/13/e8/86/13e886fbec6287235522bb2a81b35f97.jpg",
+  },
+  {
+    title: "выаав",
+    subtitle: "ыавав",
+    image:
+      "https://i.pinimg.com/736x/13/e8/86/13e886fbec6287235522bb2a81b35f97.jpg",
+  },
+  {
+    title: "выаав",
+    subtitle: "ыавав",
+    image:
+      "https://i.pinimg.com/736x/13/e8/86/13e886fbec6287235522bb2a81b35f97.jpg",
+  },
+];
 </script>
 
 <template>
@@ -9,53 +36,52 @@ const router = useRouter()
         <v-col cols="11" md="8" class="pt-8">
           <v-carousel hide-delimiters cycle style="border-radius: 8px">
             <template v-slot:prev="{ props }">
-              <v-btn class="carousel-btn" @click="props.onClick" icon="mdi-chevron-left"></v-btn>
+              <v-btn
+                class="carousel-btn"
+                @click="props.onClick"
+                icon="mdi-chevron-left"
+              ></v-btn>
             </template>
             <template v-slot:next="{ props }">
-              <v-btn class="carousel-btn" @click="props.onClick" icon="mdi-chevron-right"></v-btn>
+              <v-btn
+                class="carousel-btn"
+                @click="props.onClick"
+                icon="mdi-chevron-right"
+              ></v-btn>
             </template>
-            <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover></v-carousel-item>
-
-            <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg" cover></v-carousel-item>
-
-            <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+              cover
+            ></v-carousel-item>
+            <v-carousel-item
+              src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+              cover
+            ></v-carousel-item>
+            <v-carousel-item
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              cover
+            ></v-carousel-item>
           </v-carousel>
         </v-col>
       </v-row>
 
-      <v-row class="d-flex justify-center text-center" style="margin-top: 50px; margin-bottom: 50px;">
-        <v-col cols="11" md="8">
-          <h3 style="font-size: clamp(1.25rem, 0.7528rem + 1.4205vw, 1.875rem); color: #303030;">Мы — команда квалифицированных преподавателей. <br> У нас вы найдете индивидуальный подход, дружескую теплую атмосферу, уютные и чистые залы.</h3>
+      <v-row class="d-flex justify-center text-center mt-5 mb-5">
+        <v-col cols="12" md="10" lg="8">
+          <h3 class="headline">
+            Мы — команда квалифицированных преподавателей. <br />
+            У нас вы найдете индивидуальный подход, дружескую теплую атмосферу,
+            уютные и чистые залы.
+          </h3>
         </v-col>
       </v-row>
 
       <v-row class="d-flex justify-center">
-        <v-col cols="11" md="8">
-          <ul class="mass">
-            <li class="mass_types card" @click="router.push('/info')">
-              <img src="/assets/images/main-page-img.jpg" alt="" />
-              <h2 style="color: #666;">fibfghiuf</h2>
-              <p>gjkgjkgffj</p>
-            </li>
-            <li class="mass_types card" @click="router.push('/info')">
-              <img src="/assets/images/массаж тема лютая.jpg" alt="" />
-              <h2>fibfghiuf</h2>
-              <p>gjkgjkgffj</p>
-            </li>
-            <li class="mass_types card" @click="router.push('/info')">
-              <img src="/assets/images/массаж тема лютая.jpg" alt="" />
-              <h2>fibfghiuf</h2>
-              <p>gjkgjkgffj</p>
-            </li>
-            <li class="mass_types card" @click="router.push('/info')">
-              <img src="/assets/images/массаж тема лютая.jpg" alt="" />
-              <h2>fibfghiuf</h2>
-              <p>gjkgjkgffj</p>
-            </li>
-            <li class="mass_types card" @click="router.push('/info')">
-              <img src="/assets/images/массаж тема лютая.jpg" alt="" />
-              <h2>fibfghiuf</h2>
-              <p>gjkgjkgffj</p>
+        <v-col cols="12" sm="10" md="8">
+          <ul class="cardd">
+            <li class="cardd__ite" v-for="(item, index) in items" :key="index">
+              <img :src="item.image" alt="" />
+              <p class="title">{{ item.title }}</p>
+              <p class="subtitle">{{ item.subtitle }}</p>
             </li>
           </ul>
         </v-col>
@@ -64,63 +90,48 @@ const router = useRouter()
   </v-row>
 </template>
 <style scoped lang="scss">
-.mass {
-  gap: 10px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  padding: 0;
-  margin: 0;
+.cardd {
+  text-align: center;
   list-style: none;
-  // color: white;
-
-  // Добавляем отступы между элементами
-  li {
-    margin-bottom: 20px; // Отступ между элементами списка
-    flex: 1 1 calc(33% - 20px); // Настройка ширины элементов
-
-    @media (max-width: 768px) {
-      flex: 1 1 calc(50% - 20px); // 2 элемента в ряд на планшетах
-    }
-
-    @media (max-width: 480px) {
-      flex: 1 1 100%; // 1 элемент в ряд на мобильных устройствах
-    }
-
-    // Стилизация изображений и других элементов
-    img {
-      max-width: 100%;
-      height: auto;
-      border-radius: 8px; // Пример стилизации для изображения
-    }
-
-    h2 {
-      // font-size: 1.5rem;
-      color: #2B2B2B;
-      font-weight: 700;
-      margin: 10px;
-    }
-
-    p {
-      color: #666;
-      margin: 10px;
-    }
-  }
+  display: flex;
+  flex-wrap: wrap; // добавлено для обтекания элементов
+  justify-content: center; // центрирование элементов
+  margin: 0 -10px; // убираем отступы по бокам
 }
+
+.cardd__ite {
+  margin: 10px; // отступ между элементами
+  flex: 1 0 45%; // адаптивная ширина для карточек
+  max-width: 250px; // максимальная ширина карточки
+}
+
+img {
+  width: 100%; // изображение занимает всю ширину карточки
+  border-radius: 8px; // скругленные края изображения
+}
+
+.title {
+  font-weight: 600;
+  font-size: clamp(1rem, 1.162rem + 0.2817vw, 1.5rem);
+}
+
+.headline {
+  font-size: clamp(1.25rem, 0.7528rem + 1.4205vw, 1.875rem);
+  color: #303030;
+}
+
 .super-blur-image {
-  // background-image: url("../assets/images/background2.jpg");
   background-size: cover;
-  // background-color: #eddb99;
-
 }
+
 .card {
-  background-color: #EFDEB3;
+  background-color: #efdeb3;
   padding: 4px;
   border-radius: 8px;
   cursor: pointer;
   color: #809248;
-  // color: white;
 }
+
 .carousel-btn {
   background-color: #eddb99;
   color: #809248;
